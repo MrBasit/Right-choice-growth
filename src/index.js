ee8a2fa03ddabecd7ed71c540d9251a053f0ee22
mrbasit <itechnician030@gmail.com>
2025-08-23 04:25:35 +0500

    home page with responsiveness

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './styles.css';
 
 import { gsap } from "gsap";
 
-// Simple animation test
-gsap.to("h1", { 
-  duration: 5, 
-  y: 50, 
-  opacity: 0.5, 
-  ease: "power2.inOut" 
-});
+// // Simple animation test
+// gsap.to("h1", { 
+//   duration: 5, 
+//   y: 50, 
+//   opacity: 0.5, 
+//   ease: "power2.inOut" 
+// });
