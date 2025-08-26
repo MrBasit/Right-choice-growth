8386dbda56a0c13c89bcced313f55297c6055796
mrbasit <itechnician030@gmail.com>
2025-08-27 02:37:42 +0500

    animation and additional images

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import './styles.css';
 import { gsap } from "gsap";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
-// // Simple animation test
-// gsap.to("h1", { 
-//   duration: 5, 
-//   y: 50, 
-//   opacity: 0.5, 
-//   ease: "power2.inOut" 
-// });
+gsap.registerPlugin(ScrollTrigger,ScrollToPlugin,ScrollSmoother);
 
 document.addEventListener("DOMContentLoaded",()=>{
-    gsap.from('header nav',{
+    gsap.from('header .main-nav',{
         y:-30,
         duration:0.9
     })
@@ -39,6 +36,24 @@ document.addEventListener("DOMContentLoaded",()=>{
         delay:0
     })
 
+    let services = document.querySelectorAll('#Services .service');
+    console.log(services)
+    let servicesTimeline = gsap.timeline({
+        scrollTrigger:
+        {trigger:'#Services',
+        start:'10% 75%',
+        end: '90% 75%',
+        scrub:1}
+    })
+    servicesTimeline.from(services,{
+        scale:1.2,
+        y:-10,
+        x:-10,
+        opacity:0,
+        stagger:0.5,
+        onComplete:()=>{console.log('sroll trigger complete')}
+    })
+
     document.querySelectorAll('.btn-primary, .btn-primary-light').forEach(btn=>{
         btn.addEventListener('mouseenter',()=>{
             gsap.to(btn,{scale:1.1, duration:0.3});
@@ -62,7 +77,6 @@ document.addEventListener("DOMContentLoaded",()=>{
             popup.classList.remove('hidden');
             popup.classList.add('flex');
             iframe.src = 'https://www.youtube.com/embed/'+vid.dataset.video;
-            console.log(document.querySelector('#VideoPopup > div'));
             gsap.fromTo(
                 document.querySelector('#VideoPopup > div'),
                 {scale:0.1, opacity:0.1},
@@ -75,7 +89,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     let closeVid = document.querySelector('#VideoPopup button');
     closeVid.addEventListener('click',()=>{
         document.getElementById("ytFrame").src='';
-        console.log(document.querySelector('#VideoPopup > div'));
         gsap.fromTo(
             document.querySelector('#VideoPopup > div'),
             {scale:1, opacity:1},
@@ -92,7 +105,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         e.addEventListener("mouseenter", () => {
             gsap.to(e, { scale: 1.1, duration: 0.3, ease: "power1.out" });
             gsap.to(e, { "--color-service-bg-200": "#F3800C","--color-service-bg-300": "#8D4A07", duration: 0.3, ease: "power1.out" });
-            console.log(e.children)
             gsap.to(e.children[0], { "--color-service-icon-bg-200": "white","--color-service-icon-bg-300": "white", "--color-service-icon":"#8D4A07", duration: 0.3, ease: "power1.out" });
 
         });
@@ -108,7 +120,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     let faqButtons = document.querySelectorAll('.faq-btn');
     let testimonialNext = document.querySelector('.testimonial-next');
     let testimonialPrevious = document.querySelector('.testimonial-previous');
-
     let faqsOpened;
     
     faqButtons.forEach((e,i)=>{
@@ -201,20 +212,27 @@ document.addEventListener("DOMContentLoaded",()=>{
         let isMovementComplete = false;
         testimonials.forEach((e,i)=>{
             if(e.classList.contains('active') && !isMovementComplete){
-                let nextitem = testimonials.item(i+1 == testimonials.length ? 0 : i+1);
-                
+                let nextitem = testimonials[(i+1)%testimonials.length];
                 gsap.to(e,{
                     opacity:0,
-                    duration:1
+                    scale:0.8,
+                    x:-100,
+                    duration:0.5,
+                    onComplete:()=>{
+                        e.classList.remove('active');
+                        gsap.set(e, {
+                            opacity:1,
+                            scale:1,
+                            x:0
+                        });
+                        nextitem.classList.add('active');
+                    }
                 })
-                setTimeout(() => {
-                    e.classList.remove('active');
-                    nextitem.classList.add('active');
-                    gsap.set(e,{opacity:100});
-                }, 1000);
                 gsap.from(nextitem,{
                     opacity:0,
-                    duration:5,
+                    scale:1.2,
+                    x:150,
+                    duration:0.5,
                     delay:0.5
                 })
                 isMovementComplete = true;
@@ -228,10 +246,118 @@ document.addEventListener("DOMContentLoaded",()=>{
         testimonials.forEach((e,i)=>{
             if(e.classList.contains('active') && !isMovementComplete){
                 let nextitem = testimonials.item(i == 0 ? testimonials.length-1 : i-1);
-                e.classList.remove('active');
-                nextitem.classList.add('active');
+
+                gsap.to(e,{
+                    opacity:0,
+                    scale:0.8,
+                    x:100,
+                    duration:0.5,
+                    onComplete:()=>{
+                        e.classList.remove('active');
+                        gsap.set(e, {
+                            opacity:1,
+                            scale:1,
+                            x:0
+                        });
+                        nextitem.classList.add('active');
+                    }
+                })
+                gsap.from(nextitem,{
+                    opacity:0,
+                    scale:1.2,
+                    x:-150,
+                    duration:0.5,
+                    delay:0.5
+                })
+
                 isMovementComplete = true;
             }
         });
     });
+
+    document.querySelectorAll('.main-nav ul li a').forEach(a=>{
+        a.addEventListener('click',(e)=>{
+            e.preventDefault();
+            let sectionId = a.getAttribute('href');
+            let section = document.querySelector(sectionId);
+            console.log(sectionId,section)
+            gsap.to(window,{
+                duration:1.5,
+                scrollTo:{
+                    y:section,
+                    offsetY:0
+                },
+                ease:"power2.out"
+            })
+        })
+    })
+
+
+
+    let openMenuBtn = document.querySelector('.sec-nav .open');
+    let closeMenuBtn = document.querySelector('.sec-nav .close');
+    let mobileMenu = document.querySelector('.sec-nav ul');
+    let mobileMenuli = document.querySelectorAll('.sec-nav ul li');
+    let mobileMenuLiReverse = Array.from(document.querySelectorAll('.sec-nav ul li')).reverse();
+
+    document.querySelectorAll('.sec-nav ul li a').forEach(a=>{
+        a.addEventListener('click',(e)=>{
+            e.preventDefault();
+            let sectionId = a.getAttribute('href');
+            let section = document.querySelector(sectionId);
+
+            openMenuBtn.classList.remove('hidden');
+            closeMenuBtn.classList.add('hidden');
+            mobileMenu.classList.add('hidden');
+            
+            gsap.to(window,{
+                duration:1.5,
+                scrollTo:{
+                    y:section,
+                    offsetY:0
+                },
+                ease:"power2.out"
+            })
+        })
+    })
+
+    openMenuBtn.addEventListener('click',()=>{
+        openMenuBtn.classList.add('hidden');
+        closeMenuBtn.classList.remove('hidden');
+        mobileMenu.classList.remove('hidden');
+        gsap.to(mobileMenu,{
+            height:mobileMenu.scrollHeight,
+            duration:0.5,
+        });
+
+        gsap.from('.sec-nav ul li',{
+            y:-20,
+            opacity:0,
+            duration:0.4,
+            delay:0.25,
+            stagger:0.1,
+        });
+
+    })
+
+    closeMenuBtn.addEventListener('click',()=>{
+        closeMenuBtn.classList.add('hidden');
+        openMenuBtn.classList.remove('hidden');
+        gsap.to(mobileMenuLiReverse,{
+            y:-20,
+            opacity:0,
+            duration:0.4,
+            stagger:0.1,
+        });
+
+        gsap.to(mobileMenu,{
+            height:0,
+            duration:0.5,
+            delay:0.4,
+            onComplete:()=>{
+                mobileMenu.classList.add('hidden');
+                gsap.set(mobileMenuli,{opacity:1,y:0})
+            }
+        });
+    })
 })
